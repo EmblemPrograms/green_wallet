5d5eaa61da194c51cc49043efe902b1ec829880c
EmblemPrograms <emblemprogram08@yahoo.com>
2025-05-02 14:57:20 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:green_wallet/Card/hompage.dart';
 import 'package:green_wallet/Splash_Screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: hompage(),
-    //home: SplashScreen(),
+    home: SplashScreen(),
   ));
 }
